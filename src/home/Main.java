483c3f490232f090031e4d13835f2c123d2f71cc
indika02 <indikai002i@gmail.com>
2022-04-24 00:22:43 +0530

    login and sign up pages

--- src/home/Main.java
+++ src/home/Main.java
@@ -1,7 +1,18 @@
 package home;
 
-public class Main {
-public static void main (String args[]){
-    System.out.println("infdfd");
-}
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root= FXMLLoader.load(getClass().getResource("login.fxml"));
+        primaryStage.setTitle("Sign up");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+    public static void main(String[] args){launch(args);}
 }
---
+++ src/home/SignUp.java
@@ -0,0 +1,30 @@
+package home;
+
+import javafx.beans.Observable;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SignUp implements Initializable {
+
+    @FXML
+    private ComboBox comb;
+
+    @FXML
+    void select(ActionEvent event) {
+        String g=comb.getSelectionModel().getSelectedItem().toString();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb){
+        ObservableList<String> list= FXCollections.observableArrayList("None","Grade 06","Grade 07","Grade 08","Grade 09","Grade 10","Grade 11","Grade 12","Grade 13");
+        comb.setItems(list);
+    }
+
+}

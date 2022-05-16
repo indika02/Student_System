4663ddbe56fcebb6822aed79b977af3158b120d7
indika02 <indikai002i@gmail.com>
2022-05-17 00:27:06 +0530

    username checking

--- src/home/controller/Studentboard.java
+++ src/home/controller/Studentboard.java
@@ -2,13 +2,32 @@ package home.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.sql.*;
 
-public class Studentboard  extends Login{
+public class Studentboard{
 
 
+    @FXML
+    private Button logout;
+
+    @FXML
+    void logout(ActionEvent event) throws IOException {
+        //close current window
+        logout.getScene().getWindow().hide();
+        //move to the next window
+        Parent root= FXMLLoader.load(getClass().getResource("../ui/login.fxml"));
+        Stage mainstage=new Stage();
+        Scene scene=new Scene(root);
+        mainstage.setScene(scene);
+        mainstage.show();
+    }
 
 
 }
